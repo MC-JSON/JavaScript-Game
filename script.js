a7bc62d302753fc1cd91f8d951c7a7be6263734a
Jason McCoy <jwmccoy29@gmail.com>
2022-03-07 13:48:10 -0600

    css styling updates

---
+++ script.js
@@ -0,0 +1,16 @@
+const player = document.querySelector(input).value
+const cards = []
+const bank = 300
+const message = ''
+const blackJack = false
+const stillPlaying = false
+const playerLayout = document.getElementById('player')
+//printout playername and current bank
+
+//how to randomize cards
+
+//how to start game (ie deal cards)
+
+//render game(ie play hand)
+
+//deal new card if requested
