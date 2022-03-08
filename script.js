199b29b78643b63c1e923fcc9a53b1e0b33c036a
Jason McCoy <jwmccoy29@gmail.com>
2022-03-08 09:38:12 -0600

    update instructions html

--- script.js
+++ script.js
@@ -22,30 +22,44 @@ const faces = [
   'Three',
   'Two'
 ]
-const blackJack = false
-const stillPlaying = false
-const playerWins = false
+let blackJack = false
+let stillPlaying = false
+let playerWins = false
 const btn = document.getElementById('start')
 const bttn = document.getElementById('hit')
 const button = document.getElementById('replay')
 const cardsHand = document.getElementById('cards')
 const dealerHand = document.getElementById('dealer')
 const playerBank = document.getElementById('bank')
+let cardsHandArr = []
+let dealerHandArr = []
 playerBank.innerHTML = 'Bank ~ $' + bank
 
 //Functions below
 ///how to randomize cards to go into players/dealers hand
-const randomCard = () => {}
+const randomCard = () => {
+  let randomize = Math.floor(Math.random() * 13)
+  if (randomize > 10) {
+    return 10
+  } else if (randomize === 1) {
+    return 11
+  } else {
+    return randomize
+  }
+}
 
 ///how to start game (ie deal cards)
 const startGame = () => {
+  console.log('test')
   stillPlaying = true
   const firstCard = randomCard()
   const secondCard = randomCard()
   const thirdCard = randomCard()
   const fourthCard = randomCard()
-  cardsPlayer = [firstCard, secondCard]
-  cardsDealer = [thirdCard, fourthCard]
+  cardsHandArr.push(firstCard)
+  cardsHandArr.push(secondCard)
+  dealerHandArr.push(firstCard)
+  dealerHandArr.push(secondCard)
   sumPlayer = firstCard + secondCard
   sumDealer = thirdCard + fourthCard
   playGame()
@@ -53,7 +67,14 @@ const startGame = () => {
 
 ///render game(ie play hand) - how to add cards to screen and render messages based on sum of cards; if blackjack or higher than dealer + $100 once game reaches $1000 refer to winner.html
 const playGame = () => {
-  cardsHand.innerText = 'Cards: '
+  cardsHand.innerText = 'Cards ~ '
+  dealerHand.innerText = 'Dealer ~ '
+  for (let i = 0; i < cardsHand.length; i++) {
+    cardsHand.textContent + cardsHand[i]
+  }
+  for (let i = 0; i < dealerHand.length; i++) {
+    dealerHand.textContent + dealerHand[i]
+  }
 }
 
 ///deal new card if requested
